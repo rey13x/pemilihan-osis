df036f4a52559f659330aef230d420748087d80a
rey13x <digitawanku2@gmail.com>
2026-01-26 16:16:55 +0700

    FINISH 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
-import "./App.css";
+import "./app.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
