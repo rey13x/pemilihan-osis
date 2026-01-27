049a64f319bf16240f437f537ce832869784cf51
rey13x <digitawanku2@gmail.com>
2026-01-27 14:06:45 +0700

    Feat: Add loading text 'memuat aset' and change bar color to brown

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -64,7 +64,7 @@ export default function LoadingPage({ onLoadingComplete }) {
           />
         </div>
         
-        <p className="loading-text">Memuat aset...</p>
+        <p className="loading-text">memuat aset...</p>
       </div>
     </div>
   );
