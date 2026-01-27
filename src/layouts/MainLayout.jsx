46b0adc9aa8573b66871c155242f03f5d5da9d9a
rey13x <digitawanku2@gmail.com>
2026-01-27 08:23:10 +0700

    Fix: Remove TopBar component and resolve React Hook errors

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import TopBar from "../components/TopBar";
 import ChatBubble from "../components/ChatBubble";
 
 export default function MainLayout() {
   return (
     <>
-      <TopBar />
       <Navbar />
       <Hero />
       <Outlet />
