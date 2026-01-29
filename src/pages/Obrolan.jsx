2e9bbce57408d2a2142b6c803df9e1cad362b91a
rey13x <digitawanku2@gmail.com>
2026-01-30 01:54:17 +0700

    revisi animasi gsap & add kamera vote 6 final 3

--- src/pages/Obrolan.jsx
+++ src/pages/Obrolan.jsx
@@ -109,6 +109,7 @@ export default function Obrolan() {
       setError(err.message);
       setLoading(false);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleSendMessage = async (e) => {
